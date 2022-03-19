b9e3623491817b84ba78fb71f4484127fc1551d2
hoobert <bluewhobert@mailbox.org>
2022-03-19 13:40:16 +0100

    fix a deprecation warning

--- tests/easterformula_tests.py
+++ tests/easterformula_tests.py
@@ -7,13 +7,13 @@ class EasterFormulaTest(unittest.TestCase):
     """simple test that we can calculate the correct Easter Sunday date"""
 
     def test_calculateEasterSundayDate_for2020_calculatesRightResult(self):
-        self.assertEquals("2020-04-08", calculateEasterSundayDate(2020))
+        self.assertEqual("2020-04-08", calculateEasterSundayDate(2020))
 
     def test_calculateEasterSundayDate_for2021_calculatesRightResult(self):
-        self.assertEquals("2020-03-28", calculateEasterSundayDate(2021))
+        self.assertEqual("2020-03-28", calculateEasterSundayDate(2021))
 
     def test_calculateEasterSundayDate_for2022_calculatesRightResult(self):
-        self.assertEquals("2020-04-16", calculateEasterSundayDate(2022))
+        self.assertEqual("2020-04-16", calculateEasterSundayDate(2022))
 
 
 def main():
