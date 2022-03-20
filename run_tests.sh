6696650bc2be319a3544b889a078e6f8b5b9ef85
hoobert <bluewhobert@mailbox.org>
2022-03-20 18:01:00 +0100

    add test reports (HTML)

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -m pytest --cov=easterformula_module  --cov-report=html tests/easterformula_tests.py
+python -m pytest --cov=easterformula_module  --cov-report=html tests/easterformula_tests.py --html=report.html --self-contained-html
